74c4fa400134fcf905c9ecedec84a86e479919c4
MS Arifin <ms.arifin29@gmail.com>
2023-09-28 19:24:21 +0800

    add type string

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val firstName: String = "samsul"
+    val lasName: String = "arifin"
+    val fullName = "$firstName  ${lasName.length}"
+    val address = """
+        jl.tukad bilok no.1
+        Denpasar Bali
+    """.trimMargin()
+    println(fullName)
+    println(fullName)
+    println(address)
+}
