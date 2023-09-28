64375959342ddedf2c701a221373372223e582a0
MS Arifin <ms.arifin29@gmail.com>
2023-09-28 19:57:29 +0800

    add type range

---
+++ src/main/kotlin/Range.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val numbers = 1..100
+    println(numbers.count())
+    println(numbers.contains(50))
+    println(numbers.contains(500))
+    println(numbers.first)
+    println(numbers.last)
+    println(numbers.step)
+
+    val members = 100 downTo 1
+    println(members.count())
+    println(members.contains(50))
+    println(members.contains(500))
+    println(members.first)
+    println(members.last)
+    println(members.step)
+
+    val coins = 100 downTo 1 step 3
+    println(coins.count())
+    println(coins.contains(50))
+    println(coins.contains(500))
+    println(coins.first)
+    println(coins.last)
+    println(coins.step)
+}
