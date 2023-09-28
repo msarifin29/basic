efcec6527d4b795829bdd0fd2c0ceff5ed63e3ff
MS Arifin <ms.arifin29@gmail.com>
2023-09-28 19:16:08 +0800

    add type number

---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val byte: Byte = 127
+    val short: Short = 32767
+    val int: Int = 2147483647
+    val long: Long = 922337203685477580
+    val decimalLiteral: Int = 100
+    val hexColor: Int = 0xFF
+    val decimalisationNumb: Int = 0b010
+
+
+println(byte)
+println(short)
+println(int)
+println(long)
+    println(decimalLiteral)
+    println(hexColor)
+    println(decimalisationNumb)
+
+    val float: Float = 1.0F
+    val double: Double = 10.0
+
+    println(float)
+    println(double)
+
+
+}
