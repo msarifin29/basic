b989564a03a0aa3e70083b8557a6823438fbd16b
MS Arifin <ms.arifin29@gmail.com>
2023-10-22 19:45:58 +0800

    add comment in type variable

--- src/main/kotlin/Variable.kt
+++ src/main/kotlin/Variable.kt
@@ -1,9 +1,16 @@
+// Immutable variable global using const
 const val VERSION = 0.1
 
 fun main() {
+    // variable Mutable using var
     var name: String = "boy"
-    val nickName: String? = null
-
+    // variable Immutable (cannot be changed) using val
+    val age = 30
+    // variable Nullable
+    var nickName: String? = null
+    nickName = "paijo"
     println(name)
+    // call nullable value using key ? (question mark)
     println(nickName?.length)
+    println("$VERSION")
 }
