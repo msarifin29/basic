0f88ac1ed05f83acd1f670893ed34620d72f9e7b
MS Arifin <ms.arifin29@gmail.com>
2023-10-22 20:37:18 +0800

    add if expression

---
+++ src/main/kotlin/IfExpression.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val value = 60
+    if(value > 80){
+        println("good")
+    }else if(value > 50){
+        println("Not bad")
+    }else if(value == 100 || value > 100){
+        println("Very good")
+    }else{
+        println("Try again")
+    }
+}
