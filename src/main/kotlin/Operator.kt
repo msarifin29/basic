18f002165f35dc13f4cee4677c28c691866476eb
MS Arifin <ms.arifin29@gmail.com>
2023-10-22 20:30:29 +0800

    add type operator

---
+++ src/main/kotlin/Operator.kt
@@ -0,0 +1,16 @@
+fun main() {
+   val result = 10 + 10
+   println(result)
+   val result2 = 10 + 10 / 2 // value 15
+    // Pembagian atau perkalian akan di eksekusi terlebih dahulu
+    println(result2)
+
+    var total : Int = 0
+    val item1 = 100
+    total += item1
+    val  item2 = 200
+    total += item2
+    val item3 = 300
+    total += item3
+    println(total)
+}
