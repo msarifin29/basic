a0d79e07c7983dacec5c07d1813200ed3312da77
MS Arifin <ms.arifin29@gmail.com>
2023-10-22 19:57:05 +0800

    add comment in type array

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -1,10 +1,19 @@
 fun main() {
     val fruits: Array<String> = arrayOf("Apple","Banana","Lemon")
+    // get index of array
     fruits[1] = "Strawberry"
+    // get length of array
+    fruits.size
+    // set value in array
+    fruits.set(0, "Lemon")
+    // or
+    fruits[0] = "Orange"
+
     println(fruits.size)
     println(fruits[0])
     println(fruits[1])
 
+    // Array nullable
 val members: Array<Int?> = arrayOfNulls(3)
     members[0] = 0
     members[1] = 1
